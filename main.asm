a68d30de29f41ec383c5958c1fbb7f879b303604
chomp105 <54012005+chomp105@users.noreply.github.com>
2022-04-04 13:22:36 -0500

    updated lable names

--- main.asm
+++ main.asm
@@ -51,10 +51,10 @@ clrloop:
 	pop ecx				; restore ecx reg value
 	ret				; return
 
-	; The chkaction function compares action with the commands
+	; The ParseAction function compares action with the commands
 	; It puts the id of any match in the eax reg
 
-chkaction:
+ParseAction:
 	push ebx			; save ebx reg value
 	push ecx			; save ecx reg value
 	push edx			; save edx reg value
@@ -64,43 +64,43 @@ chkaction:
 	xor ecx, ecx			; zero out ecx reg
 	xor edx, edx			; zero out edx reg
 	mov edi, action			; move action address into edi reg
-chkloop:
+ParseActionLoop:
 	mov al, [edi + ecx]		; move nth byte of action into al reg
 	cmp [ebx + ecx], byte 10	; compare commands + ecx to 10
-	jl chkloopend			; if commands + ecx equals 10, end
+	jl ParseLoopEnd			; if commands + ecx equals 10, end
 	cmp al, 32			; compare al to 32
-	je nextaction			; if al equals 32, return
+	je ParseActionNextAction	; if al equals 32, return
 	cmp al, 10			; cmp al to 10
-	je exit				; if al equals 10, return
+	je ParseActionEnd		; if al equals 10, return
 	cmp al, [ebx + ecx]		; compare al reg to nth byte of commands
-	jne nextcmd			; jump to next command if not equal	
+	jne ParseActionNextCommand	; jump to next command if not equal	
 	inc ecx				; increment ecx reg (loop counter)	
-	jmp chkloop			; loop	
-chkloopend:
+	jmp ParseActionLoop		; loop	
+ParseActionLoopEnd:
 	cmp [ebx+ecx], byte 0		; compare commands + ecx reg to 0
-	jne chkloopres			; if commands + ecx reg doesn't equal 0, return
+	jne ParseLoopResult		; if commands + ecx reg doesn't equal 0, return
 	inc ecx				; increment ecx
-	jmp chkloopend
-chkloopres:	
+	jmp ParseActionEnd
+ParseActionResult:	
 	movzx eax, byte [ebx+ecx] 	; move the command id to eax
-	jmp chkend			; jump to chkend
-nextcmd:
+	jmp ParseActionEnd		; jump to chkend
+ParseActionNextCommand:
 	mov edx, commands		; move commands address into edx reg
 	add edx, 30			; add 31 to edx reg
 	cmp ebx, edx			; compare ebx reg to edx reg
-	je nextaction			; if ebx equals edx, jump to chkend
+	je ParseActionNextAction	; if ebx equals edx, jump to chkend
 	add ebx, 6			; add 6 to ebx reg
 	xor ecx, ecx			; zero out ecx reg
-	jmp chkloop			; jump to chkloop
-nextaction:
+	jmp ParseActionLoop		; jump to chkloop
+ParseActionNextAction:
 	add edi, ecx			; add ecx reg to edi reg
 	inc edi				; increment edi reg
 	cmp [edi], byte 0		; compare edi with 0
-	je chkend			; if edi equals 0, return
+	je ParseActionEnd		; if edi equals 0, return
 	xor ecx, ecx			; zero out ecx reg
 	mov ebx, commands		; mov commands into ebx reg
-	jmp chkloop			; jump to chkloop
-chkend:
+	jmp ParseActionLoop		; jump to chkloop
+ParseActionEnd:
 	pop edi
 	pop edx				; restore edx reg value
 	pop ecx				; restore ecx reg value
